bf28a4cd44e738880a225dfd0ad4f9d6b93d2164
dreherk <k.dreher@dkfz-heidelberg.de>
2019-02-25 13:24:30 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name="ippai",
+    version="0.0.1",
+    author="Computer Assisted Medical Interventions, DKFZ",
+    description="Image Processing for Photoacoustic Imaging",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy"
+    ]
+)
