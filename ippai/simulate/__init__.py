a503f1734a16e56f1ce4bf59d8c25f0cb91fa4ea
Janek Gröhl <j.groehl@dkfz-heidelberg.de>
2019-03-19 12:26:39 +0100

    create Tag class with static constants

--- ippai/simulate/__init__.py
+++ ippai/simulate/__init__.py
@@ -1,21 +1,42 @@
+class Tags:
 
-from ippai.simulate.volume_creator import create_simulation_volume
-from ippai.simulate.optical_model import run_optical_forward_model
-from ippai.simulate.acoustic_model import run_acoustic_forward_model
+    # General settings
+    SIMULATION_PATH = "simulation_path"
+    WAVELENGTH = "wavelength"
+
+    # Model settings
+    RUN_OPTICAL_MODEL = 'run_optical_forward_model'
+    RUN_ACOUSTIC_MODEL = 'run_acoustic_forward_model'
+
+    # Volume geometry settings
+    SPACING = "voxel_spacing_mm"
+    DIM_VOLUME_X = "volume_x_dim_mm"
+    DIM_VOLUME_Y = "volume_y_dim_mm"
+    DIM_VOLUME_Z = "volume_z_dim_mm"
+
+    # Tissue Properties Settings
+    KEY_B_MIN = "B_min"
+    KEY_B_MAX = "B_max"
+    KEY_W_MAX = "w_max"
+    KEY_W_MIN = "w_min"
+    KEY_F_MAX = "f_max"
+    KEY_F_MIN = "f_min"
+    KEY_M_MAX = "m_max"
+    KEY_M_MIN = "m_min"
+    KEY_OXY_MAX = "oxy_max"
+    KEY_OXY_MIN = "oxy_min"
+
+    # Structures
+    STRUCTURES = "structures"
+    STRUCTURE_TYPE = "structure_type"
+    STRUCTURE_LAYER = "structure_layer"
+    STRUCTURE_LAYER_DEPTH_MIN = "structure_depth_min_mm"
+    STRUCTURE_LAYER_DEPTH_MAX = "structure_depth_max_mm"
+    STRUCTURE_LAYER_THICKNESS_MIN = "structure_thickness_min_mm"
+    STRUCTURE_LAYER_THICKNESS_MAX = "structure_thickness_max_mm"
+    STRUCTURE_TISSUE_PROPERTIES = "structure_tissue_properties"
 
 
-def simulate(settings):
-    volume_path = create_simulation_volume(settings)
-    optical_path = None
-    acoustic_path = None
-
-    if settings['run_optical_forward_model']:
-        optical_path = run_optical_forward_model(settings, volume_path)
-
-    if settings['run_acoustic_forward_model']:
-        acoustic_path = run_acoustic_forward_model(settings, optical_path)
-
-    return [volume_path, optical_path, acoustic_path]
 
 
 
