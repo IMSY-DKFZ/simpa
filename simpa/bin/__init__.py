fcbaba499cc347c89e7a6eba166b4db9f6c31b01
Leonardo Menjivar <l.menjivar@dkfz-heidelberg.de>
2020-12-04 14:03:08 +0100

    Basic layout of SAM dash app

---
+++ simpa/bin/__init__.py
@@ -0,0 +1,54 @@
+"""
+Hyperspectral organ database dimensionality reduction
+===================================================================
+The MIT License (MIT)
+
+Copyright (c) 2020 Computer Assisted Medical Interventions Group
+German Cancer Research Center
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+REQUIREMENTS:
+===================================================================
+You will need to install the following packages in order to use this script. It is recommended to create a virtualenv
+to install this packages:
+
+``
+
+USAGE:
+===================================================================
+
+"""
+
+import logging
+import argparse
+
+
+def main(**kwargs):
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script name")
+    parser.add_argument('-h', '--help', required=False, action="store_true",
+                        help='Prints help information for this script')
+    args = vars(parser.parse_args())
+    logging.basicConfig(filename="log.txt", level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.info(f"-------------------\nStarted script with args: {args}")
+    main(**args)
