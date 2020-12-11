a5075f658acf6c0db40d9fbb39bac73820593375
Leonardo Menjivar <l.menjivar@dkfz-heidelberg.de>
2020-12-11 17:31:39 +0100

    restructured layout and added first callback for plot11

--- simpa/bin/dash_simulations.py
+++ simpa/bin/dash_simulations.py
@@ -41,24 +41,33 @@ import dash_core_components as dcc
 import dash_table
 import dash_bootstrap_components as dbc
 import dash_html_components as html
+import dash_colorscales as dcs
+import plotly_express as px
+import plotly.graph_objects as go
 import argparse
 import base64
+import numpy as np
 
 
 external_stylesheets = [dbc.themes.JOURNAL, '.assets/dcc.css']
 app = dash.Dash(external_stylesheets=external_stylesheets, title="SIMPA")
 
 simpa_logo = './.assets/simpa_logo.png'
-encoded_logo = (base64.b64encode(open(simpa_logo, 'rb').read())).decode()
+cami_logo = './.assets/CAMIC_logo-wo_DKFZ.png'
+encoded_simpa_logo = (base64.b64encode(open(simpa_logo, 'rb').read())).decode()
+encoded_cami_logo = (base64.b64encode(open(cami_logo, 'rb').read())).decode()
 
 app.layout = html.Div([
     dbc.Row([
         dbc.Col([
             html.H4("SIMPA Visualization Tool"),
             html.H6("CAMI, Computer Assisted Medical Interventions"),
-        ], width=9),
+        ], width=6),
         dbc.Col([
-            html.Img(src='data:image/png;base64,{}'.format(encoded_logo), width='50%')
+            html.Img(src='data:image/png;base64,{}'.format(encoded_simpa_logo), width='50%')
+        ], width=3),
+        dbc.Col([
+            html.Img(src='data:image/png;base64,{}'.format(encoded_cami_logo), width='50%')
         ], width=3)
     ]),
     html.Br(),
@@ -86,30 +95,36 @@ app.layout = html.Div([
                     html.Br(),
                     html.Hr(),
                     html.H6("Data selection"),
-                    dbc.Input(
-                        id="n_plots",
-                        type="number",
-                        pattern=None,
-                        placeholder="Number of plots",
-                        persistence_type="session",
-                        disabled=True,
-                    ),
                     dcc.Dropdown(
-                        id="param",
-                        multi=True,
-                        placeholder="Parameter to plot",
+                        id="volume_axis",
+                        multi=False,
+                        placeholder="Volume axis",
                         persistence_type="session",
-                        disabled=True,
+                        options=[{'label': 'x', 'value': 'x'},
+                                 {'label': 'y', 'value': 'y'},
+                                 {'label': 'z', 'value': 'z'}],
+                        value='z'
                     ),
                     html.Br(),
                     html.Hr(),
                     html.H6("Visual settings"),
-                    dcc.Dropdown(
-                        id="palette_chooser",
-                        multi=False,
-                        placeholder="Color palette",
+                    html.P("Color scale"),
+                    dcs.DashColorscales(
+                        id="colorscale_picker",
+                        nSwatches=7,
+                        fixSwatches=True
+                    ),
+                    html.P("Limit plotted values as %"),
+                    dcc.RangeSlider(
+                        id="plot_scaler",
+                        min=0,
+                        max=100,
+                        value=[10, 90],
+                        marks={i: {'label': str(i)} for i in list(range(101))[::10]},
+                        allowCross=False,
+                        pushable=5,
+                        tooltip=dict(always_visible=True, placement="top"),
                         persistence_type="session",
-                        disabled=True,
                     )
                 ], width=2),
                 dbc.Col([
@@ -124,7 +139,77 @@ app.layout = html.Div([
                                               style={"width": "50%", "display": 'inline-block'}),
                                     dcc.Graph(id="plot_12",
                                               hoverData={'points': [{'x': 0, 'y': 0, 'customdata': None}]},
-                                              style={"width": "50%", "display": 'inline-block'})
+                                              style={"width": "50%", "display": 'inline-block'}),
+                                    html.Div([
+                                        dcc.Dropdown(
+                                            id="plot_type1",
+                                            multi=False,
+                                            placeholder="Plot type",
+                                            persistence_type="session",
+                                            disabled=True,
+                                            style=dict(width='150px', display='inline-block')
+                                        ),
+                                        dcc.Dropdown(
+                                            id="param1",
+                                            multi=False,
+                                            placeholder="Parameter to plot",
+                                            persistence_type="session",
+                                            disabled=True,
+                                            style={'width': '150px', 'display': 'inline-block', 'margin-left': '5px'}
+                                        ),
+                                        html.H6("Volume slice selector"),
+                                        dcc.Slider(
+                                            min=0,
+                                            max=100,
+                                            value=50,
+                                            step=1,
+                                            tooltip=dict(always_visible=True, placement="top"),
+                                            updatemode="mouseup",
+                                            persistence_type="session",
+                                            id="volume_slider",
+                                            marks={
+                                                0: {'label': '0', 'style': {'color': '#77b0b1'}},
+                                                26: {'label': '26'},
+                                                37: {'label': '37'},
+                                                100: {'label': '100', 'style': {'color': '#f50'}}
+                                            }
+                                        )
+                                    ], style={"width": "50%", "display": 'inline-block'}),
+                                    html.Div([
+                                        dcc.Dropdown(
+                                            id="plot_type2",
+                                            multi=False,
+                                            placeholder="Plot type",
+                                            persistence_type="session",
+                                            disabled=True,
+                                            style=dict(width='150px', display='inline-block')
+                                        ),
+                                        dcc.Dropdown(
+                                            id="param2",
+                                            multi=False,
+                                            placeholder="Parameter to plot",
+                                            persistence_type="session",
+                                            disabled=True,
+                                            style={'width': '150px', 'display': 'inline-block', 'margin-left': '5px'}
+                                        ),
+                                        html.H6("Wavelength selector"),
+                                        dcc.Slider(
+                                            min=0,
+                                            max=100,
+                                            value=50,
+                                            step=1,
+                                            tooltip=dict(always_visible=True, placement="top"),
+                                            updatemode="mouseup",
+                                            persistence_type="session",
+                                            id="channel_slider",
+                                            marks={
+                                                0: {'label': '0', 'style': {'color': '#77b0b1'}},
+                                                26: {'label': '26'},
+                                                37: {'label': '37'},
+                                                100: {'label': '100', 'style': {'color': '#f50'}}
+                                            }
+                                        )
+                                    ], style={"width": "50%", "display": 'inline-block'})
                                 ]
                             )
                         ])
@@ -136,6 +221,13 @@ app.layout = html.Div([
                             html.Div(
                                 id="plot_div_2",
                                 children=[
+                                    dcc.Dropdown(
+                                        id="param3",
+                                        multi=False,
+                                        placeholder="Parameter to plot over wavelengths",
+                                        persistence_type="session",
+                                        disabled=True,
+                                    ),
                                     dcc.Graph(id="plot_21",
                                               hoverData={'points': [{'x': 0, 'y': 0, 'customdata': None}]},
                                               style={"width": "100%", "display": 'inline-block'})
@@ -196,6 +288,48 @@ app.layout = html.Div([
         ])
     ])
 ], style={'padding': 40})
+
+
+class DataContainer(object):
+    plot_types = ['scatter3d', 'imshow', 'histogram-volume', 'histograme-channel', 'box', 'violin', 'contour']
+    wavelengths = np.arange(500, 1000, 5)
+    vol_shape = (60, 60, 60)
+    X, Y, Z = np.mgrid[:1:20j, :1:20j, :1:20j]
+    vol = (X - 1) ** 2 + (Y - 1) ** 2 + Z ** 2
+    data = (X, Y, Z, vol)
+
+
+data_container = DataContainer()
+
+
+@app.callback(Output("plot_11", "figure"),
+              [Input("plot_type1", "value"),
+               Input("param1", "value"),
+               Input("colorscale_picker", "colorscale")])
+def update_plot11(plot_type, param, colorscale):
+    if plot_type == 'scatter3d':
+        if len(data_container.vol_shape) == 3:
+            x, y, z, v = data_container.data[param]
+            fig = go.Volume(
+                x=x,
+                y=y,
+                z=z,
+                v=v,
+                opacity=0.2,
+                colorscale=colorscale
+            )
+            return fig
+        else:
+            return {"data": []}
+    elif plot_type == "histogram-volume":
+        x, y, z, v = data_container.data
+        if isinstance(v, np.ndarray):
+            fig = px.histogram(v)
+            return fig
+        else:
+            return {"data": []}
+    else:
+        return {"data": []}
 
 
 if __name__ == "__main__":
