26e2eb2e4af54cc5fa894d1893d41c7cc81aa092
Tom Rix <tom.rix@dkfz-heidelberg.de>
2021-03-05 17:13:23 +0100

    Restored testing volume

--- simpa_tests/simulation_tests/PyTorchDASReconstruction.py
+++ simpa_tests/simulation_tests/PyTorchDASReconstruction.py
@@ -39,10 +39,10 @@ class PyTorchDASReconstruction():
         """
         print("setUp")
 
-        SAVE_PATH = "D:/save/"
-        MCX_BINARY_PATH = "D:/bin/Release/mcx.exe"  # On Linux systems, the .exe at the end must be omitted.
-        MATLAB_PATH = "C:/Program Files/MATLAB/R2020b/bin/matlab.exe"
-        ACOUSTIC_MODEL_SCRIPT = "C:/simpa/simpa/core/acoustic_simulation"
+        SAVE_PATH = "/home/tom/dev/FP/simpa/simpa_examples"
+        MCX_BINARY_PATH = "../../simpa_examples/mcx"  # On Linux systems, the .exe at the end must be omitted.
+        MATLAB_PATH = "/usr/local/MATLAB/R2020b/bin/matlab"
+        ACOUSTIC_MODEL_SCRIPT = "/home/tom/dev/FP/simpa/simpa/core/acoustic_simulation"
 
         self.VOLUME_TRANSDUCER_DIM_IN_MM = 75
         self.VOLUME_PLANAR_DIM_IN_MM = 20
@@ -117,6 +117,32 @@ class PyTorchDASReconstruction():
     def tearDown(self):
         print("tearDown")
 
+    def test_reconstruction_of_simulation(self):
+        print("Simulating ", self.RANDOM_SEED)
+        import time
+        timer = time.time()
+        simulate(self.settings)
+        print("Needed", time.time() - timer, "seconds")
+        print("Simulating ", self.RANDOM_SEED, "[Done]")
+
+        reconstructed_image_path = generate_dict_path(
+            Tags.RECONSTRUCTED_DATA,
+            wavelength=self.settings[Tags.WAVELENGTH])
+
+        reconstructed_image = load_hdf5(
+            self.settings[Tags.SIMPA_OUTPUT_PATH],
+            reconstructed_image_path)[Tags.RECONSTRUCTED_DATA]
+
+        initial_pressure = load_data_field(self.settings[Tags.SIMPA_OUTPUT_PATH], Tags.OPTICAL_MODEL_INITIAL_PRESSURE, wavelength=self.settings[Tags.WAVELENGTH])
+
+        plt.subplot(1, 2, 1)
+        plt.title("Reconstructed image")
+        plt.imshow(np.log10(np.abs(reconstructed_image)))
+        plt.subplot(1, 2, 2)
+        plt.title("Initial pressure")
+        plt.imshow(np.log10(initial_pressure))
+        plt.show()
+
     def create_example_tissue(self):
         """
         This is a very simple example script of how to create a tissue definition.
@@ -147,118 +173,36 @@ class PyTorchDASReconstruction():
         vessel_1_dictionary[Tags.CONSIDER_PARTIAL_VOLUME] = True
         vessel_1_dictionary[Tags.STRUCTURE_TYPE] = Tags.CIRCULAR_TUBULAR_STRUCTURE
 
-        epidermis_dictionary = Settings()
-        epidermis_dictionary[Tags.PRIORITY] = 8
-        epidermis_dictionary[Tags.STRUCTURE_START_MM] = [0, 0, 0]
-        epidermis_dictionary[Tags.STRUCTURE_END_MM] = [0, 0, 1]
-        epidermis_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.epidermis()
-        epidermis_dictionary[Tags.CONSIDER_PARTIAL_VOLUME] = True
-        epidermis_dictionary[Tags.ADHERE_TO_DEFORMATION] = True
-        epidermis_dictionary[Tags.STRUCTURE_TYPE] = Tags.HORIZONTAL_LAYER_STRUCTURE
+        vessel_2_dictionary = Settings()
+        vessel_2_dictionary[Tags.PRIORITY] = 3
+        vessel_2_dictionary[Tags.STRUCTURE_START_MM] = [self.VOLUME_TRANSDUCER_DIM_IN_MM / 2 + 12,
+                                                        0, 7]
+        vessel_2_dictionary[Tags.STRUCTURE_END_MM] = [self.VOLUME_TRANSDUCER_DIM_IN_MM / 2 + 10,
+                                                      self.VOLUME_PLANAR_DIM_IN_MM, 5]
+        vessel_2_dictionary[Tags.STRUCTURE_RADIUS_MM] = 3
+        vessel_2_dictionary[Tags.STRUCTURE_ECCENTRICITY] = 0.9
+        vessel_2_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.blood_generic()
+        vessel_2_dictionary[Tags.CONSIDER_PARTIAL_VOLUME] = True
+        vessel_2_dictionary[Tags.STRUCTURE_TYPE] = Tags.ELLIPTICAL_TUBULAR_STRUCTURE
+
+        vessel_3_dictionary = Settings()
+        vessel_3_dictionary[Tags.PRIORITY] = 3
+        vessel_3_dictionary[Tags.STRUCTURE_START_MM] = [self.VOLUME_TRANSDUCER_DIM_IN_MM / 2 - 12,
+                                                        0, 3]
+        vessel_3_dictionary[Tags.STRUCTURE_X_EXTENT_MM] = 8
+        vessel_3_dictionary[Tags.STRUCTURE_Y_EXTENT_MM] = 10
+        vessel_3_dictionary[Tags.STRUCTURE_Z_EXTENT_MM] = 16
+        vessel_3_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.blood_generic()
+        vessel_3_dictionary[Tags.CONSIDER_PARTIAL_VOLUME] = True
+        vessel_3_dictionary[Tags.STRUCTURE_TYPE] = Tags.RECTANGULAR_CUBOID_STRUCTURE
 
         tissue_dict = Settings()
         tissue_dict[Tags.BACKGROUND] = background_dictionary
         tissue_dict["muscle"] = muscle_dictionary
-        tissue_dict["epidermis"] = epidermis_dictionary
         tissue_dict["vessel_1"] = vessel_1_dictionary
+        tissue_dict["vessel_2"] = vessel_2_dictionary
+        tissue_dict["vessel_3"] = vessel_3_dictionary
         return tissue_dict
-
-    def test_reconstruction_of_simulation(self):
-        print("Simulating ", self.RANDOM_SEED)
-        import time
-        timer = time.time()
-        simulate(self.settings)
-        print("Needed", time.time() - timer, "seconds")
-        print("Simulating ", self.RANDOM_SEED, "[Done]")
-
-        reconstructed_image_path = generate_dict_path(
-            Tags.RECONSTRUCTED_DATA,
-            wavelength=self.settings[Tags.WAVELENGTH])
-
-        reconstructed_image = load_hdf5(
-            self.settings[Tags.SIMPA_OUTPUT_PATH],
-            reconstructed_image_path)[Tags.RECONSTRUCTED_DATA]
-
-        initial_pressure = load_data_field(self.settings[Tags.SIMPA_OUTPUT_PATH], Tags.OPTICAL_MODEL_INITIAL_PRESSURE, wavelength=self.settings[Tags.WAVELENGTH])
-
-        plt.subplot(1, 2, 1)
-        plt.title("Reconstructed image")
-        plt.imshow(np.log10(np.abs(reconstructed_image)))
-        plt.subplot(1, 2, 2)
-        plt.title("Initial pressure")
-        plt.imshow(np.log10(initial_pressure))
-        plt.show()
-
-    # def create_example_tissue(self):
-    #     """
-    #     This is a very simple example script of how to create a tissue definition.
-    #     It contains a muscular background, an epidermis layer on top of the muscles
-    #     and a blood vessel.
-    #     """
-    #     background_dictionary = Settings()
-    #     background_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.muscle()
-    #     background_dictionary[Tags.STRUCTURE_TYPE] = Tags.BACKGROUND
-    #
-    #     muscle_dictionary = Settings()
-    #     muscle_dictionary[Tags.PRIORITY] = 1
-    #     muscle_dictionary[Tags.STRUCTURE_START_MM] = [0, 0, 0]
-    #     muscle_dictionary[Tags.STRUCTURE_END_MM] = [0, 0, 100]
-    #     muscle_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.muscle()
-    #     muscle_dictionary[Tags.CONSIDER_PARTIAL_VOLUME] = True
-    #     muscle_dictionary[Tags.ADHERE_TO_DEFORMATION] = True
-    #     muscle_dictionary[Tags.STRUCTURE_TYPE] = Tags.HORIZONTAL_LAYER_STRUCTURE
-    #
-    #     vessel_1_dictionary = Settings()
-    #     vessel_1_dictionary[Tags.PRIORITY] = 3
-    #     vessel_1_dictionary[Tags.STRUCTURE_START_MM] = [self.VOLUME_TRANSDUCER_DIM_IN_MM / 2,
-    #                                                     0, 10]
-    #     vessel_1_dictionary[Tags.STRUCTURE_END_MM] = [self.VOLUME_TRANSDUCER_DIM_IN_MM / 2,
-    #                                                   self.VOLUME_PLANAR_DIM_IN_MM, 10]
-    #     vessel_1_dictionary[Tags.STRUCTURE_RADIUS_MM] = 3
-    #     vessel_1_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.blood_generic()
-    #     vessel_1_dictionary[Tags.CONSIDER_PARTIAL_VOLUME] = True
-    #     vessel_1_dictionary[Tags.STRUCTURE_TYPE] = Tags.CIRCULAR_TUBULAR_STRUCTURE
-    #
-    #     vessel_2_dictionary = Settings()
-    #     vessel_2_dictionary[Tags.PRIORITY] = 3
-    #     vessel_2_dictionary[Tags.STRUCTURE_START_MM] = [self.VOLUME_TRANSDUCER_DIM_IN_MM / 2 + 12,
-    #                                                     0, 7]
-    #     vessel_2_dictionary[Tags.STRUCTURE_END_MM] = [self.VOLUME_TRANSDUCER_DIM_IN_MM / 2 + 10,
-    #                                                   self.VOLUME_PLANAR_DIM_IN_MM, 5]
-    #     vessel_2_dictionary[Tags.STRUCTURE_RADIUS_MM] = 3
-    #     vessel_2_dictionary[Tags.STRUCTURE_ECCENTRICITY] = 0.9
-    #     vessel_2_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.blood_generic()
-    #     vessel_2_dictionary[Tags.CONSIDER_PARTIAL_VOLUME] = True
-    #     vessel_2_dictionary[Tags.STRUCTURE_TYPE] = Tags.ELLIPTICAL_TUBULAR_STRUCTURE
-    #
-    #     vessel_3_dictionary = Settings()
-    #     vessel_3_dictionary[Tags.PRIORITY] = 3
-    #     vessel_3_dictionary[Tags.STRUCTURE_START_MM] = [self.VOLUME_TRANSDUCER_DIM_IN_MM / 2 - 12,
-    #                                                     0, 3]
-    #     vessel_3_dictionary[Tags.STRUCTURE_X_EXTENT_MM] = 8
-    #     vessel_3_dictionary[Tags.STRUCTURE_Y_EXTENT_MM] = 10
-    #     vessel_3_dictionary[Tags.STRUCTURE_Z_EXTENT_MM] = 16
-    #     vessel_3_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.blood_generic()
-    #     vessel_3_dictionary[Tags.CONSIDER_PARTIAL_VOLUME] = True
-    #     vessel_3_dictionary[Tags.STRUCTURE_TYPE] = Tags.RECTANGULAR_CUBOID_STRUCTURE
-    #
-    #     epidermis_dictionary = Settings()
-    #     epidermis_dictionary[Tags.PRIORITY] = 8
-    #     epidermis_dictionary[Tags.STRUCTURE_START_MM] = [0, 0, 0]
-    #     epidermis_dictionary[Tags.STRUCTURE_END_MM] = [0, 0, 1]
-    #     epidermis_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.epidermis()
-    #     epidermis_dictionary[Tags.CONSIDER_PARTIAL_VOLUME] = True
-    #     epidermis_dictionary[Tags.ADHERE_TO_DEFORMATION] = True
-    #     epidermis_dictionary[Tags.STRUCTURE_TYPE] = Tags.HORIZONTAL_LAYER_STRUCTURE
-    #
-    #     tissue_dict = Settings()
-    #     tissue_dict[Tags.BACKGROUND] = background_dictionary
-    #     tissue_dict["muscle"] = muscle_dictionary
-    #     #tissue_dict["epidermis"] = epidermis_dictionary
-    #     tissue_dict["vessel_1"] = vessel_1_dictionary
-    #     tissue_dict["vessel_2"] = vessel_2_dictionary
-    #     tissue_dict["vessel_3"] = vessel_3_dictionary
-    #     return tissue_dict
 
 
 if __name__ == '__main__':
