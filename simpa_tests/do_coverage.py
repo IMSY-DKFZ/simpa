1617be276dfe71d91c52d245be854a3948681e14
Janek Gröhl <janek.grohl@cruk.cam.ac.uk>
2021-03-05 11:12:06 +0000

    remove the tissue library tests

--- simpa_tests/do_coverage.py
+++ simpa_tests/do_coverage.py
@@ -37,7 +37,6 @@ test_classes = ["simpa_tests.framework_tests.structure_tests.TestLayers",
                 "simpa_tests.framework_tests.TestCreateSettings",
                 "simpa_tests.framework_tests.TestIOHandling",
                 "simpa_tests.framework_tests.TestCalculationUtils",
-                "simpa_tests.framework_tests.TestTissueLibrary",
                 "simpa_tests.framework_tests.TestMoleculeLibrary"
                 ]
 
