01b36d0d57d4dbf461139f768942b18e9421113e
JAnek Gröhl <janek.grohl@cruk.cam.ac.uk>
2021-03-22 10:03:34 +0000

    added first implementation stubs for a singleton logger

---
+++ simpa/log/file_logger.py
@@ -0,0 +1,49 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 Computer Assisted Medical Interventions Group, DKFZ
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated simpa_documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import logging
+
+
+class Logger():
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Logger, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        logging.basicConfig(filename="simpa.log", level=logging.DEBUG,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+
+    def debug(self, msg, *args):
+        logging.debug(msg, *args)
+
+
+if __name__ =="__main__":
+    logger = Logger()
+    logger.debug("Test")
+
+    logger2 = Logger()
+    logger2.debug("Test")
+
+    print(logger is logger2)
