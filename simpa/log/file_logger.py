791918596fb4e0be035c7ffebd7103fd5194b0f6
Janek Gröhl <janek.grohl@cruk.cam.ac.uk>
2021-04-09 09:56:15 +0100

    updated docstring

--- simpa/log/file_logger.py
+++ simpa/log/file_logger.py
@@ -29,7 +29,15 @@ class Logger:
     The SIMPA Logger.
     The purpose of this class is to guarantee that the logging Config has been set and that logging strings are written
     to the same file throughout the entire simulation pipeline.
-    Per default, the log file is
+    Per default, the log file is located in the home directory as defined by Path.home().
+
+    The log levels are defined the same way they are in the python logging module:
+    DEBUG: Detailed information, typically of interest only when diagnosing problems.
+    INFO: Confirmation that things are working as expected.
+    WARNING: An indication that something unexpected happened, or indicative of some problem in the near future
+    (e.g. ‘disk space low’). The software is still working as expected.
+    ERROR: Due to a more serious problem, the software has not been able to perform some function.
+    CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
     """
     _instance = None
     _simpa_logging_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -66,16 +74,47 @@ class Logger:
         return cls._instance
 
     def debug(self, msg):
+        """
+        Logs a debug message to the logging system.
+        DEBUG: Detailed information, typically of interest only when diagnosing problems.
+
+        :param msg: the message to log
+        """
         self._logger.debug(msg)
 
     def info(self, msg):
+        """
+        Logs an info message to the logging system.
+        INFO: Confirmation that things are working as expected.
+
+        :param msg: the message to log
+        """
         self._logger.info(msg)
 
     def warning(self, msg):
+        """
+        Logs a warning message to the logging system.
+        WARNING: An indication that something unexpected happened, or indicative of some problem
+        in the near future (e.g. ‘disk space low’). The software is still working as expected.
+
+        :param msg: the message to log
+        """
         self._logger.warning(msg)
 
     def error(self, msg):
+        """
+        Logs an error message to the logging system.
+        ERROR: Due to a more serious problem, the software has not been able to perform some function.
+
+        :param msg: the message to log
+        """
         self._logger.error(msg)
 
     def critical(self, msg):
+        """
+        Logs a critical message to the logging system.
+        CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+
+        :param msg: the message to log
+        """
         self._logger.critical(msg)
