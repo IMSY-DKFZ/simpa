364931d21b6ec2d21ca924aea5ca82bfa88d176f
Janek Grohl <janek.grohl@cruk.cam.ac.uk>
2021-05-27 23:19:16 +0100

    TestDiffuseFluence is executable again, but resuts are off now.

--- simpa_tests/simulation_tests/TestDiffuseFluence.py
+++ simpa_tests/simulation_tests/TestDiffuseFluence.py
@@ -5,14 +5,35 @@ SPDX-License-Identifier: MIT
 """
 
 import unittest
-from simpa.utils import Tags
+from simpa.utils import Tags, PathManager, Settings, TISSUE_LIBRARY
+from simpa.core.simulation import simulate
+from simpa.core import VolumeCreationModelModelBasedAdapter, OpticalForwardModelMcxAdapter
+from simpa.core.device_digital_twins import PhotoacousticDevice, PencilBeamIlluminationGeometry
+from simpa.io_handling import load_data_field
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+# FIXME temporary workaround for newest Intel architectures
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 
-@unittest.skip("skipping local simulation tests")
+#@unittest.skip("skipping local simulation tests")
 class TestInifinitesimalSlabExperiment(unittest.TestCase):
+
+    def create_example_tissue(self):
+        """
+        This is a very simple example script of how to create a tissue definition.
+        It contains a muscular background, an epidermis layer on top of the muscles
+        and two blood vessels. It is used for volume creation.
+        """
+        background_dictionary = Settings()
+        background_dictionary[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.constant(self.mua, self.mus, self.g)
+        background_dictionary[Tags.STRUCTURE_TYPE] = Tags.BACKGROUND
+        tissue_dict = Settings()
+        tissue_dict[Tags.BACKGROUND] = background_dictionary
+
+        return tissue_dict
 
     def setUp(self):
         """
@@ -21,53 +42,57 @@ class TestInifinitesimalSlabExperiment(unittest.TestCase):
         :return:
         """
 
-        SIMULATION_PATH = "/path/to/save/location"
-        MCX_BINARY_PATH = "/path/to/mcx/binary/mcx.exe"     # On Linux systems, the .exe at the end must be omitted.
-        VOLUME_NAME = "TestVolume"
+        path_manager = PathManager()
+        self.dim = 100
 
-        self.settings = {
+        self.settings = Settings({
             Tags.WAVELENGTHS: [800],
             Tags.WAVELENGTH: 800,
-            Tags.VOLUME_NAME: VOLUME_NAME,
-            Tags.SIMULATION_PATH: SIMULATION_PATH,
-            Tags.RUN_OPTICAL_MODEL: True,
-            Tags.OPTICAL_MODEL_NUMBER_PHOTONS: 1e7,
-            Tags.OPTICAL_MODEL_BINARY_PATH: MCX_BINARY_PATH,
-            Tags.RUN_ACOUSTIC_MODEL: False,
+            Tags.VOLUME_NAME: "DiffuseFluenceTest",
+            Tags.SIMULATION_PATH: path_manager.get_hdf5_file_save_path(),
             Tags.SPACING_MM: 1,
-            Tags.OPTICAL_MODEL: Tags.OPTICAL_MODEL_MCX,
-            Tags.PROPERTY_ANISOTROPY: 0.9,
-            Tags.ILLUMINATION_TYPE: Tags.ILLUMINATION_TYPE_PENCIL,
-            Tags.SIMPA_OUTPUT_PATH: "simpa_tests.hdf5"
-        }
-
-        self.volume_path = self.settings[Tags.SIMULATION_PATH] + "/"+ self.settings[Tags.VOLUME_NAME] + "/" \
-                           + self.settings[Tags.VOLUME_NAME] + "_" + str(self.settings[Tags.WAVELENGTH]) + ".npz"
-
-        folder_name = self.settings[Tags.SIMULATION_PATH] + "/"+ self.settings[Tags.VOLUME_NAME] + "/"
-        if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
-
-        self.dim = 100
-        self.volume = np.zeros((self.dim, self.dim, self.dim, 4))
+            Tags.DIM_VOLUME_X_MM: self.dim,
+            Tags.DIM_VOLUME_Y_MM: self.dim,
+            Tags.DIM_VOLUME_Z_MM: self.dim,
+            Tags.RANDOM_SEED: 4711
+        })
 
         self.mua = 0.1
         self.mus = 100
         self.g = 0.9
 
-        self.volume[:, :, :, 0] = self.mua
-        self.volume[:, :, :, 1] = self.mus
-        self.volume[:, :, :, 2] = self.g
-        self.volume[:, :, :, 3] = 1
+        self.settings.set_volume_creation_settings({
+            Tags.SIMULATE_DEFORMED_LAYERS: True,
+            Tags.STRUCTURES: self.create_example_tissue()
+        })
+        self.settings.set_optical_settings({
+            Tags.OPTICAL_MODEL_NUMBER_PHOTONS: 1e7,
+            Tags.OPTICAL_MODEL_BINARY_PATH: path_manager.get_mcx_binary_path(),
+            Tags.OPTICAL_MODEL: Tags.OPTICAL_MODEL_MCX,
+            Tags.ILLUMINATION_TYPE: Tags.ILLUMINATION_TYPE_PENCIL,
+            Tags.LASER_PULSE_ENERGY_IN_MILLIJOULE: 50
+        })
+
+        class CustomDevice(PhotoacousticDevice):
+
+            def __init__(self):
+                super(CustomDevice, self).__init__()
+                self.add_illumination_geometry(PencilBeamIlluminationGeometry())
+
+            def get_default_probe_position(self, global_settings: Settings) -> np.ndarray:
+                return np.asarray([global_settings[Tags.DIM_VOLUME_X_MM] / 2,
+                                   global_settings[Tags.DIM_VOLUME_Y_MM] / 2, 0])
+
+        self.device = CustomDevice()
 
     def test_fluence(self):
-        self.perform_test(distance=self.dim/2, spacing=1)
+        self.perform_test(distance=self.dim, spacing=1)
 
     def test_spacing_short(self):
-        self.perform_test(distance=5, spacing=0.1)
+        self.perform_test(distance=self.dim, spacing=0.333333)
 
     def test_spacing_middle(self):
-        self.perform_test(distance=self.dim/4, spacing=0.5)
+        self.perform_test(distance=self.dim, spacing=0.5)
 
     def test_spacing_long(self):
         self.perform_test(distance=self.dim, spacing=2)
@@ -80,15 +105,10 @@ class TestInifinitesimalSlabExperiment(unittest.TestCase):
         :param r: radial distance between source and detector.
         :return: fluence at a point with source-detector distance r.
         """
-        if self.settings[Tags.OPTICAL_MODEL] == Tags.OPTICAL_MODEL_MCX:
-            mua = 0.1 * self.mua    # convert mua from cm^-1 to mm^-1
-            mus = 0.1 * self.mus    # convert mus from cm^-1 to mm^-1
-            spacing = self.settings[Tags.SPACING_MM]
-        else:
-            r = r / 10.0
-            mua = self.mua
-            mus = self.mus
-            spacing = self.settings[Tags.SPACING_MM] / 10.0
+
+        mua = 0.1 * self.mua    # convert mua from cm^-1 to mm^-1
+        mus = 0.1 * self.mus    # convert mus from cm^-1 to mm^-1
+        spacing = self.settings[Tags.SPACING_MM]
 
         mus_prime = (1-self.g) * mus
         mu_tot = mus_prime + mua
@@ -109,34 +129,33 @@ class TestInifinitesimalSlabExperiment(unittest.TestCase):
         # fluence
         phi = 1 / (4*np.pi*D) * (np.exp(-mu_eff*r1) / r1 - np.exp(-mu_eff*r2) / r2)
 
-        if self.settings[Tags.OPTICAL_MODEL] == Tags.OPTICAL_MODEL_MCXYZ:
-            phi = phi / 100
-
         return phi
 
     def perform_test(self, distance, spacing):
 
         self.settings[Tags.SPACING_MM] = spacing
 
-        np.savez(self.volume_path,
-                 mua=self.volume[:, :, :, 0],
-                 mus=self.volume[:, :, :, 1],
-                 g=self.volume[:, :, :, 2],
-                 gamma=self.volume[:, :, :, 3]
-                 )
+        # run pipeline including volume creation and optical mcx simulation
+        pipeline = [
+            VolumeCreationModelModelBasedAdapter(self.settings),
+            OpticalForwardModelMcxAdapter(self.settings),
+        ]
+        simulate(pipeline, self.settings, self.device)
 
-        self.assertDiffusionTheory(distance)
+        self.assertDiffusionTheory(distance, spacing)
 
-    def assertDiffusionTheory(self, distance):
-        optical_path = None#run_optical_forward_model(self.settings)
-        fluence = np.load(optical_path)['fluence']
-        number_of_measurements = np.arange(1, int(distance/self.settings[Tags.SPACING_MM]) + 1, 1)
+    def assertDiffusionTheory(self, distance, spacing):
+        fluence = load_data_field(self.settings[Tags.SIMPA_OUTPUT_PATH], Tags.OPTICAL_MODEL_FLUENCE,
+                                  self.settings[Tags.WAVELENGTH])
+        print(np.shape(fluence))
+        number_of_measurements = np.arange(0, int(distance/self.settings[Tags.SPACING_MM]), 1)
+        print(number_of_measurements)
         measurement_distances = number_of_measurements * self.settings[Tags.SPACING_MM]
-        fluence_measurements = fluence[int(self.dim/2), (int(self.dim/2) - 1 + number_of_measurements), 0]
+        fluence_measurements = fluence[int((self.dim/spacing)/2), int((self.dim/spacing)/2), number_of_measurements]
 
-        fluence_measurements = fluence_measurements / 100
+        fluence_measurements = fluence_measurements / 1000
 
-        diffusion_approx = self.diff_theory_fluence(measurement_distances)
+        diffusion_approx = self.diff_theory_fluence(measurement_distances + 1)
 
         # Plot the results:
 
