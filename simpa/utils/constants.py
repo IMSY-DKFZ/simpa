84e6717c6c75306ba7c7a0015df756b8fe0b2477
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2021-10-29 12:13:03 +0200

    Removed old SaveFIlePaths

--- simpa/utils/constants.py
+++ simpa/utils/constants.py
@@ -4,27 +4,10 @@ SPDX-FileCopyrightText: 2021 VISION Lab, Cancer Research UK Cambridge Institute 
 SPDX-License-Identifier: MIT
 """
 
-from simpa.utils import Tags
-
-
 EPS = 1e-20
 """
 Defines the smallest increment that should be considered by SIMPA.
 """
-
-
-class SaveFilePaths:
-    """
-    The save file paths specify the path of a specific data structure in the dictionary of the simpa output hdf5.
-    All of these paths have to be used like:
-    SaveFilePaths.PATH + "data_structure"
-    """
-    SIMULATION_PROPERTIES = "/" + Tags.SIMULATIONS + "/" + Tags.SIMULATION_PROPERTIES + "/"
-    OPTICAL_OUTPUT = "/" + Tags.SIMULATIONS + "/" + Tags.OPTICAL_MODEL_OUTPUT_NAME + "/"
-    ACOUSTIC_OUTPUT = "/" + Tags.SIMULATIONS + "/" + Tags.TIME_SERIES_DATA + "/"
-    NOISE_ACOUSTIC_OUTPUT = "/" + Tags.SIMULATIONS + "/" + Tags.TIME_SERIES_DATA_NOISE + "/"
-    RECONSTRCTION_OUTPUT = "/" + Tags.SIMULATIONS + "/" + Tags.RECONSTRUCTED_DATA + "/"
-    NOISE_RECONSTRCTION_OUTPUT = "/" + Tags.SIMULATIONS + "/" + Tags.RECONSTRUCTED_DATA_NOISE + "/"
 
 
 class SegmentationClasses:
