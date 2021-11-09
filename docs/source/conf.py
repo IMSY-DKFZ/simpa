3b6572637b82dfcfe83f0064a78a1f7b03fcb58a
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2021-11-09 14:51:24 +0100

    Corrected documentation file locations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ SPDX-License-Identifier: MIT
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- Project information -----------------------------------------------------
