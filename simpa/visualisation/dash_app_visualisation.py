353656d05cc4ebcd423f4473265f2dade7e68c05
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2021-12-15 15:29:57 +0100

    added warning for dash app

--- simpa/visualisation/dash_app_visualisation.py
+++ simpa/visualisation/dash_app_visualisation.py
@@ -1,37 +1,24 @@
+# SPDX-FileCopyrightText: 2021 Computer Assisted Medical Interventions Group, DKFZ
+# SPDX-FileCopyrightText: 2021 Janek Groehl
+# SPDX-License-Identifier: MIT
+
 """
-Hyperspectral tissue simulation visualization toolkit
+WARNING!:
 ===================================================================
-The MIT License (MIT)
-
-Copyright (c) 2018 Computer Assisted Medical Interventions Group, DKFZ
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated simpa_documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+This dash app visualisation is highly experimental and under development.
+Feel free to use it, however, we can not yet guarantee that everything works perfectly.
+Therefore, we would greatly appreciate your feedback!
 
 REQUIREMENTS:
 ===================================================================
 You will need to install the following packages in order to use this script. It is recommended to create a virtualenv
-to install this packages:
+to install the specified requirements in the dash_app_requirements.txt file in this folder by:
 
-`pip install dash dash-table dash_colorscales plotly plotly-express dash-bootstrap-components pandas numpy`
+`pip install -r dash_app_requirements.txt`
 
 USAGE:
 ===================================================================
+run `python dash_app_visualisation`
 
 """
 import warnings
