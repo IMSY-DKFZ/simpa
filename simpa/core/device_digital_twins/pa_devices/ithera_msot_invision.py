de31d5dcc51ccb43be2868882d669d73d3bff4b7
Janek Grohl <janek.grohl@cruk.cam.ac.uk>
2022-04-28 09:23:13 +0100

    add iThera InVision definition as used in the MSOT file format

--- simpa/core/device_digital_twins/pa_devices/ithera_msot_invision.py
+++ simpa/core/device_digital_twins/pa_devices/ithera_msot_invision.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 import numpy as np
 
-from simpa.core.device_digital_twins import PhotoacousticDevice, CurvedArrayDetectionGeometry, \
+from simpa.core.device_digital_twins import PhotoacousticDevice, iTheraInvision256TFDetectionGeometry, \
     MSOTInVisionIlluminationGeometry
 
 
@@ -40,17 +40,8 @@ class InVision256TF(PhotoacousticDevice):
         super(InVision256TF, self).__init__(device_position_mm=device_position_mm,
                                             field_of_view_extent_mm=field_of_view_extent_mm)
 
-        detection_geometry = CurvedArrayDetectionGeometry(pitch_mm=0.735,
-                                                          radius_mm=40,
-                                                          number_detector_elements=256,
-                                                          detector_element_width_mm=0.635,
-                                                          detector_element_length_mm=15,
-                                                          center_frequency_hz=5e6,
-                                                          bandwidth_percent=55,
-                                                          sampling_frequency_mhz=40,
-                                                          angular_origin_offset=0,
-                                                          device_position_mm=device_position_mm,
-                                                          field_of_view_extent_mm=field_of_view_extent_mm)
+        detection_geometry = iTheraInvision256TFDetectionGeometry(device_position_mm=device_position_mm,
+                                                                  field_of_view_extent_mm=field_of_view_extent_mm)
 
         self.field_of_view_extent_mm = detection_geometry.field_of_view_extent_mm
         self.set_detection_geometry(detection_geometry)
