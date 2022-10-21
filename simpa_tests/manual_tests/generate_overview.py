db47cf9acc6f195a69ca8b3a204d78f66bfe6881
Bender, Christoph Julien <christophjulien.bender@dkfz-heidelberg.de>
2022-10-21 13:53:58 +0200

    able to automatically get all classes and to run it and first .md creation steps

--- simpa_tests/manual_tests/generate_overview.py
+++ simpa_tests/manual_tests/generate_overview.py
@@ -5,19 +5,20 @@
 import os
 import sys
 import inspect
-#import unittest
+#import unittest # TODO: delete
 from mdutils.mdutils import MdUtils
 from simpa_tests import manual_test_classes
 import simpa_tests.manual_tests.acoustic_forward_models.KWaveAcousticForwardConvenienceFunction as specific_module
 import simpa_tests.manual_tests as test_module
 import ast
-
-#mdFile = MdUtils(file_name='manual_tests_overview', title='Overview of Manual Test Results')
+import importlib
 
 # Run all manual tests automatically and save the figures
 
 class GenerateOverview_unittest():
     """
+    TODO: delete
+    deprecated
     geht nicht da die manual tests keine unittest objects sind
     """
 
@@ -34,13 +35,18 @@ class GenerateOverview_unittest():
 
 class GenerateOverview():
     """
-    Assumes that the class is named like the file itself.
+
     """
 
     def __init__(self):
         self.import_path = "simpa_tests.manual_tests"
-        self.current_dir = os.path.dirname(os.path.realpath(__file__))
+        self.current_dir = os.path.dirname(os.path.realpath(__file__)) # directory of this script, i.e ~/simpa/simpa_tests/manual_tests
+        self.save_path = os.path.join(self.current_dir, "figures/")
         self.file_name = os.path.basename(__file__)
+
+        if not os.path.isdir(self.save_path):
+            print(f"Created {self.save_path} directory")
+            os.mkdir(self.save_path)
 
     
     def clean_dir(self, dir):
@@ -59,64 +65,60 @@ class GenerateOverview():
                 pass 
 
     def run_manual_tests(self):
+        """
+        
+        """
 
         directories = os.listdir(self.current_dir)
-
         self.clean_dir(directories)
  
-        #print(dir(test_module))
-
-        #print(dir(specific_module))
-
         for dir_ in directories:
+            print()
+            print(dir_)
             files = os.listdir(os.path.join(self.current_dir, dir_))
-
-            self.clean_dir(files)#
-
+            self.clean_dir(files)
             
             for file in files:
-
+                print()
+                print(" ", file)
                 # execute all manual test scripts
                 global_path = os.path.join(self.current_dir, dir_, file)
+                
                 if False:
-                    
+                    # to run the script
                     os.system(f"python3 {global_path}")
+                
 
+                module_name = ".".join([self.import_path, dir_, file.split(".")[0]])
 
+                if False:
+                    module = __import__(module_name)
+                else:
+                    module = importlib.import_module(module_name)
 
+                # run all test classes of the current python source code
                 with open(global_path, 'r', encoding='utf-8') as source:
                     p = ast.parse(source.read())
                     classes = [node.name for node in ast.walk(p) if isinstance(node, ast.ClassDef)]
-                    print(classes)
-                # ['test', 'test2', 'inner_class']
+                    for class_name in classes:
+                        print("   Run", class_name)
+                        class_ = getattr(module, class_name)
 
+                        # run the manual test
+                        test_object = class_()
+                        test_object.run_test(show_figure_on_screen=False)#, save_path=self.save_path) # TODO: uncomment
+                break
+            break  
 
+    def create_pdf(self):
+        mdFile = MdUtils(file_name='manual_tests_overview', title='Overview of Manual Test Results')
+        mdFile.new_header(level=1, title='Overview of Manual Test results') 
 
-                #class_name = os.path.splitext(file)[0]       
-                #module_name = ".".join([self.import_path, dir_, class_name])
-                
-                #print(module_name)
-                #
-                #print(module_name)
-                #print(dir(module_name))
-                #print()
-
-
-                #module = __import__(module_name, fromlist=[class_name])
-                #class_object = getattr(module, class_name)
-                #print("\n\n\nRun ", class_name)
-                       
-                #print(sys.modules[module_name])
-                #for name, obj in inspect.getmembers(sys.modules[module_name]):
-                    #if inspect.isclass(obj):
-                        #print(obj)
-                
-                
-                #test = class_object()
-                #test.run_test(show_figure_on_screen=False)
-
+        print("saving md file in os.getcwd()=", os.getcwd())
+        mdFile.create_md_file()
 
 
 if __name__ == '__main__':
     automatic_manual_tests = GenerateOverview()
     automatic_manual_tests.run_manual_tests()
+    #automatic_manual_tests.create_pdf()
