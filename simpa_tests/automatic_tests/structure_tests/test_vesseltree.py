f29dacd897add4db8fe0e206b8b17b32287b7245
frisograce <frisograce@gmail.com>
2024-05-28 11:28:05 +0200

    More libraries needed, changes to initial structure to make the vessel start in the centre of the volume, with very basic features

--- simpa_tests/automatic_tests/structure_tests/test_vesseltree.py
+++ simpa_tests/automatic_tests/structure_tests/test_vesseltree.py
@@ -4,6 +4,8 @@
 
 import unittest
 import numpy as np
+import torch
+from skimage import measure
 from simpa.utils.libraries.tissue_library import TISSUE_LIBRARY
 from simpa.utils import Tags
 from simpa.utils.settings import Settings
@@ -15,18 +17,18 @@ class TestVesselTree(unittest.TestCase):
     def setUp(self):
         self.global_settings = Settings()
         self.global_settings[Tags.SPACING_MM] = 1
-
-        self.global_settings[Tags.DIM_VOLUME_X_MM] = 5
-        self.global_settings[Tags.DIM_VOLUME_Y_MM] = 5
-        self.global_settings[Tags.DIM_VOLUME_Z_MM] = 5
+        self.global_settings[Tags.RANDOM_SEED] = 42
+        self.global_settings[Tags.DIM_VOLUME_X_MM] = 10
+        self.global_settings[Tags.DIM_VOLUME_Y_MM] = 10
+        self.global_settings[Tags.DIM_VOLUME_Z_MM] = 10
 
         self.vesseltree_settings = Settings()
-        self.vesseltree_settings[Tags.STRUCTURE_START_MM] = [0, 0, 0]
-        self.vesseltree_settings[Tags.STRUCTURE_RADIUS_MM] = 5
+        self.vesseltree_settings[Tags.STRUCTURE_START_MM] = [5, 0, 5]
+        self.vesseltree_settings[Tags.STRUCTURE_RADIUS_MM] = 2
         self.vesseltree_settings[Tags.STRUCTURE_DIRECTION] = [0, 1, 0]
-        self.vesseltree_settings[Tags.STRUCTURE_BIFURCATION_LENGTH_MM] = 5
-        self.vesseltree_settings[Tags.STRUCTURE_RADIUS_VARIATION_FACTOR] = 1.0
-        self.vesseltree_settings[Tags.STRUCTURE_CURVATURE_FACTOR] = 1.0
+        self.vesseltree_settings[Tags.STRUCTURE_BIFURCATION_LENGTH_MM] = 20
+        self.vesseltree_settings[Tags.STRUCTURE_RADIUS_VARIATION_FACTOR] = 0
+        self.vesseltree_settings[Tags.STRUCTURE_CURVATURE_FACTOR] = 0
         self.vesseltree_settings[Tags.MOLECULE_COMPOSITION] = TISSUE_LIBRARY.muscle()
         self.vesseltree_settings[Tags.ADHERE_TO_DEFORMATION] = True
         self.vesseltree_settings[Tags.CONSIDER_PARTIAL_VOLUME] = True
