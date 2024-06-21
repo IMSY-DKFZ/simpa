bb294ef4ce064d5a0c0fb91ac631a0b4c0ded9f8
Friso Grace <friso.grace@dkfz-heidelberg.de>
2024-06-21 09:45:32 +0200

    Making profiling produce documents, rather than printing in the terminal

--- simpa/utils/profiling.py
+++ simpa/utils/profiling.py
@@ -5,6 +5,10 @@
 import os
 
 profile_type = os.getenv("SIMPA_PROFILE")
+if os.getenv("SIMPA_PROFILE_SAVE_FILE"):
+    stream = open(os.getenv("SIMPA_PROFILE_SAVE_FILE"), 'w')
+else:
+    stream = None
 if profile_type is None:
     # define a no-op @profile decorator
     def profile(f):
@@ -14,16 +18,33 @@ elif profile_type == "TIME":
     from line_profiler import LineProfiler
 
     profile = LineProfiler()
-    atexit.register(profile.print_stats)
+
+    def print_stats_atexit():
+        return profile.print_stats(stream=stream, output_unit=10**(-6))
+    atexit.register(print_stats_atexit)
 elif profile_type == "MEMORY":
     from memory_profiler import profile
+    profile = profile(stream=stream)
 elif profile_type == "GPU_MEMORY":
-    from pytorch_memlab import profile
-    from torch.cuda import memory_summary
+    from typing import Tuple
+    from pytorch_memlab.line_profiler.line_profiler import LineProfiler, DEFAULT_COLUMNS
     import atexit
 
-    @atexit.register
-    def print_memory_summary():
-        print(memory_summary())
+    global_line_profiler = LineProfiler()
+    global_line_profiler.enable()
+
+    def profile(func, columns: Tuple[str, ...] = DEFAULT_COLUMNS):
+        """
+        Check plagiarism problems
+        """
+        import atexit
+        global_line_profiler.add_function(func)
+
+        def print_stats_atexit():
+            global_line_profiler.print_stats(func, columns, stream=stream)
+
+        atexit.register(print_stats_atexit)
+        return func
+
 else:
     raise RuntimeError("SIMPA_PROFILE env var is defined but invalid: valid values are TIME, MEMORY, or GPU_MEMORY")
