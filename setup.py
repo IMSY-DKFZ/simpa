3d919fd95bdb6999528df4db57ce3c3d62d6d793
Jan-Hinrich Nölke <j.noelke@dkfz.de>
2024-06-25 17:48:26 +0200

    setup.py can be removed as we don't need compatability with legacy builds

--- setup.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2021 Division of Intelligent Medical Systems, DKFZ
-# SPDX-FileCopyrightText: 2021 Janek Groehl
-# SPDX-License-Identifier: MIT
-
-from setuptools import setup
-
-setup()
