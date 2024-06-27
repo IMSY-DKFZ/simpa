ceceb121ca7e0867a535f05c25319aa69e8d04a2
Tom Rix <tom.rix@dkfz-heidelberg.de>
2024-06-27 17:02:48 +0200

    Default global settings added to PipelineModule

--- simpa/core/__init__.py
+++ simpa/core/__init__.py
@@ -5,7 +5,7 @@ from abc import abstractmethod
 
 from simpa.core.device_digital_twins import DigitalDeviceTwinBase
 from simpa.log import Logger
-from simpa.utils import Settings
+from simpa.utils import Settings, Tags, PathManager
 from simpa.utils.processing_device import get_processing_device
 
 class PipelineModule:
@@ -18,8 +18,30 @@ class PipelineModule:
          :type global_settings: Settings
         """
         self.logger = Logger()
-        self.global_settings = global_settings
+        self.global_settings = self.get_default_global_settings()
+        self.global_settings.update(global_settings) # add and if necessary overrides default global settings by user given settings
         self.torch_device = get_processing_device(self.global_settings)
+
+    def get_default_global_settings(self) -> Settings:
+        """Return the default global settings
+
+        :return: default global settings
+        :rtype: Settings
+        """
+        path_manager = PathManager()
+
+        default_global_settings = {
+            Tags.RANDOM_SEED: 4711,
+            Tags.VOLUME_NAME: "CompletePipelineExample_4711",
+            Tags.SIMULATION_PATH: path_manager.get_hdf5_file_save_path(),
+            Tags.GPU: True,
+            Tags.DO_FILE_COMPRESSION: True,
+            Tags.DO_IPASC_EXPORT: True,
+            Tags.CONTINUE_SIMULATION: False 
+        }
+
+        return Settings(default_global_settings)
+
 
     @abstractmethod
     def run(self, digital_device_twin: DigitalDeviceTwinBase):
