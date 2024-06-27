275d21822a2951537023b97c318261a0bfc3df89
Jan-Hinrich Nölke <j.noelke@dkfz.de>
2024-06-27 15:10:03 +0200

    add simpa version to hdf5 file
    Co-authored-by: Jonas Burian <jonas.burian@protonmail.com>

--- simpa/core/simulation.py
+++ simpa/core/simulation.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 from simpa.utils import Tags
+from simpa import __version__
 from simpa.io_handling.io_hdf5 import save_hdf5, load_hdf5, save_data_field, load_data_field
 from simpa.io_handling.ipasc import export_to_ipasc
 from simpa.utils.settings import Settings
@@ -56,6 +57,7 @@ def simulate(simulation_pipeline: list, settings: Settings, digital_device_twin:
 
     settings[Tags.SIMPA_OUTPUT_PATH] = simpa_output_path + ".hdf5"
 
+    simpa_output[Tags.SIMPA_VERSION] = __version__
     simpa_output[Tags.SETTINGS] = settings
     simpa_output[Tags.DIGITAL_DEVICE] = digital_device_twin
     simpa_output[Tags.SIMULATION_PIPELINE] = [type(x).__name__ for x in simulation_pipeline]
