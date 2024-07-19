812b4620a7509850507c82b1fab914dd3b8f4033
Tom Rix <tom.rix@dkfz-heidelberg.de>
2024-07-19 14:54:09 +0200

    Refactored (base) classes from init.py into separate files

---
+++ simpa/core/simulation_modules/reconstruction_module/reconstruction_module_base_adapter.py
@@ -0,0 +1,95 @@
+# SPDX-FileCopyrightText: 2021 Division of Intelligent Medical Systems, DKFZ
+# SPDX-FileCopyrightText: 2021 Janek Groehl
+# SPDX-License-Identifier: MIT
+
+from simpa.utils import Tags
+from simpa.core.device_digital_twins import DetectionGeometryBase
+from simpa.core.device_digital_twins import PhotoacousticDevice
+from simpa.io_handling.io_hdf5 import load_data_field
+from abc import abstractmethod
+from simpa.core.simulation_modules import SimulationModule
+from simpa.utils.dict_path_manager import generate_dict_path
+from simpa.io_handling.io_hdf5 import save_hdf5
+import numpy as np
+from simpa.utils import Settings
+from simpa.core.simulation_modules.reconstruction_module.reconstruction_utils import bandpass_filter_with_settings, apply_b_mode
+from simpa.utils.quality_assurance.data_sanity_testing import assert_array_well_defined
+
+
+class ReconstructionAdapterBase(SimulationModule):
+    """
+    This class is the main entry point to perform image reconstruction using the SIMPA toolkit.
+    All information necessary for the respective reconstruction method must be contained in the
+    respective settings dictionary.
+    """
+
+    def __init__(self, global_settings: Settings):
+        super(ReconstructionAdapterBase, self).__init__(global_settings=global_settings)
+
+    def load_component_settings(self) -> Settings:
+        """Implements abstract method to serve reconstruction settings as component settings
+
+        :return: Settings: reconstruction component settings
+        """
+        return self.global_settings.get_reconstruction_settings()
+
+    @abstractmethod
+    def reconstruction_algorithm(self, time_series_sensor_data,
+                                 detection_geometry: DetectionGeometryBase) -> np.ndarray:
+        """
+        A deriving class needs to implement this method according to its model.
+
+        :param time_series_sensor_data: the time series sensor data
+        :param detection_geometry:
+        :return: a reconstructed photoacoustic image
+        """
+        pass
+
+    def run(self, device):
+        self.logger.info("Performing reconstruction...")
+
+        time_series_sensor_data = load_data_field(self.global_settings[Tags.SIMPA_OUTPUT_PATH],
+                                                  Tags.DATA_FIELD_TIME_SERIES_DATA, self.global_settings[Tags.WAVELENGTH])
+
+        _device = None
+        if isinstance(device, DetectionGeometryBase):
+            _device = device
+        elif isinstance(device, PhotoacousticDevice):
+            _device = device.get_detection_geometry()
+        else:
+            raise TypeError(f"Type {type(device)} is not supported for performing image reconstruction.")
+
+        if Tags.RECONSTRUCTION_PERFORM_BANDPASS_FILTERING in self.component_settings and \
+                self.component_settings[Tags.RECONSTRUCTION_PERFORM_BANDPASS_FILTERING]:
+
+            time_series_sensor_data = bandpass_filter_with_settings(time_series_sensor_data,
+                                                                    self.global_settings,
+                                                                    self.component_settings,
+                                                                    _device)
+
+        # check for B-mode methods and perform envelope detection on time series data if specified
+        if Tags.RECONSTRUCTION_BMODE_BEFORE_RECONSTRUCTION in self.component_settings \
+                and self.component_settings[Tags.RECONSTRUCTION_BMODE_BEFORE_RECONSTRUCTION] \
+                and Tags.RECONSTRUCTION_BMODE_METHOD in self.component_settings:
+            time_series_sensor_data = apply_b_mode(
+                time_series_sensor_data, method=self.component_settings[Tags.RECONSTRUCTION_BMODE_METHOD])
+
+        reconstruction = self.reconstruction_algorithm(time_series_sensor_data, _device)
+
+        # check for B-mode methods and perform envelope detection on time series data if specified
+        if Tags.RECONSTRUCTION_BMODE_AFTER_RECONSTRUCTION in self.component_settings \
+                and self.component_settings[Tags.RECONSTRUCTION_BMODE_AFTER_RECONSTRUCTION] \
+                and Tags.RECONSTRUCTION_BMODE_METHOD in self.component_settings:
+            reconstruction = apply_b_mode(
+                reconstruction, method=self.component_settings[Tags.RECONSTRUCTION_BMODE_METHOD])
+
+        if not (Tags.IGNORE_QA_ASSERTIONS in self.global_settings and Tags.IGNORE_QA_ASSERTIONS):
+            assert_array_well_defined(reconstruction, array_name="reconstruction")
+
+        reconstruction_output_path = generate_dict_path(
+            Tags.DATA_FIELD_RECONSTRUCTED_DATA, self.global_settings[Tags.WAVELENGTH])
+
+        save_hdf5(reconstruction, self.global_settings[Tags.SIMPA_OUTPUT_PATH],
+                  reconstruction_output_path)
+
+        self.logger.info("Performing reconstruction...[Done]")
---
+++ simpa/core/simulation_modules/volume_creation_module/volume_creator_base_adapter.py
@@ -0,0 +1,78 @@
+# SPDX-FileCopyrightText: 2021 Division of Intelligent Medical Systems, DKFZ
+# SPDX-FileCopyrightText: 2021 Janek Groehl
+# SPDX-License-Identifier: MIT
+
+from abc import abstractmethod
+from simpa.utils.settings import Settings
+from simpa.utils import Tags
+from simpa.utils.constants import wavelength_independent_properties, property_tags
+import torch
+from simpa.core.simulation_modules import SimulationModule
+from simpa.io_handling import save_data_field
+from simpa.utils.quality_assurance.data_sanity_testing import assert_equal_shapes, assert_array_well_defined
+
+
+class VolumeCreatorModuleBase(SimulationModule):
+    """
+    Use this class to define your own volume creation adapter.
+
+    """
+
+    def __init__(self, global_settings: Settings):
+        super(VolumeCreatorModuleBase, self).__init__(global_settings=global_settings)
+
+    def load_component_settings(self) -> Settings:
+        """Implements abstract method to serve volume creation settings as component settings
+
+        :return: Settings: volume creation component settings
+        """
+        return self.global_settings.get_volume_creation_settings()
+
+    def create_empty_volumes(self):
+        volumes = dict()
+        voxel_spacing = self.global_settings[Tags.SPACING_MM]
+        volume_x_dim = int(round(self.global_settings[Tags.DIM_VOLUME_X_MM] / voxel_spacing))
+        volume_y_dim = int(round(self.global_settings[Tags.DIM_VOLUME_Y_MM] / voxel_spacing))
+        volume_z_dim = int(round(self.global_settings[Tags.DIM_VOLUME_Z_MM] / voxel_spacing))
+        sizes = (volume_x_dim, volume_y_dim, volume_z_dim)
+
+        wavelength = self.global_settings[Tags.WAVELENGTH]
+        first_wavelength = self.global_settings[Tags.WAVELENGTHS][0]
+
+        for key in property_tags:
+            # Create wavelength-independent properties only in the first wavelength run
+            if key in wavelength_independent_properties and wavelength != first_wavelength:
+                continue
+            volumes[key] = torch.zeros(sizes, dtype=torch.float, device=self.torch_device)
+
+        return volumes, volume_x_dim, volume_y_dim, volume_z_dim
+
+    @abstractmethod
+    def create_simulation_volume(self) -> dict:
+        """
+        This method creates an in silico representation of a tissue as described in the settings file that is given.
+
+        :return: A dictionary containing optical and acoustic properties as well as other characteristics of the
+            simulated volume such as oxygenation, and a segmentation mask. All of these are given as 3d numpy arrays.
+        :rtype: dict
+        """
+        pass
+
+    def run(self, device):
+        self.logger.info("VOLUME CREATION")
+
+        volumes = self.create_simulation_volume()
+        # explicitly empty cache to free reserved GPU memory after volume creation
+        torch.cuda.empty_cache()
+
+        if not (Tags.IGNORE_QA_ASSERTIONS in self.global_settings and Tags.IGNORE_QA_ASSERTIONS):
+            assert_equal_shapes(list(volumes.values()))
+            for _volume_name in volumes.keys():
+                if _volume_name == Tags.DATA_FIELD_OXYGENATION:
+                    # oxygenation can have NaN by definition
+                    continue
+                assert_array_well_defined(volumes[_volume_name], array_name=_volume_name)
+
+        for key, value in volumes.items():
+            save_data_field(value, self.global_settings[Tags.SIMPA_OUTPUT_PATH],
+                            data_field=key, wavelength=self.global_settings[Tags.WAVELENGTH])
